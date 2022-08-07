8f50c3760a1e44bd2932f49380845ebfa3a0eab0
Raviraj Choudhary <rajravi78600@gmail.com>
2022-08-07 07:42:34 +0530

    fourth

--- Router/web.js
+++ Router/web.js
@@ -1,18 +1,11 @@
 import express from 'express'
+
 import UserController from '../Controller/userController.js'
 import SignupController from '../Controller/singUpController.js'
-import LoginIdController from '../Controller/GeoCheckin.js'
-import UserIdController from '../Controller/GetData.js'
-import PropsController from '../Controller/Fetchdata.js'
-import LeaveController from '../Controller/FetchLeave.js'
-import LeavesController from '../Controller/LeaveController.js'
+
 import GetAttendanceController from '../Controller/GetAttendance.js'
 import ExpenseController from '../Controller/sendExpense.js'
-import SendTask from '../Controller/SendTask.js'
-import getTaskController from '../Controller/GetTask.js'
-import getExpenseController from '../Controller/GetExpense.js'
-import sendlocController from '../Controller/SendLocation.js'
-import signoutController from '../Controller/SignOut.js'
+
 import GetDataaController from '../Controller/GetDataa.js'
 
 const router = express.Router()
